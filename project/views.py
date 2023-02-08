129e5774921795b17244c7ff65ba42e3d6502357
Pasha-js <Pashakhurtenko@gmail.com>
2023-02-08 14:03:41 +0300

    Wp config

--- project/views.py
+++ project/views.py
@@ -5,6 +5,5 @@ from django.shortcuts import render
 
 
 def index(request):
-
     context = {}
-    return render(request, 'project/index.html', context)
+    return render(request, 'index.html', context)
